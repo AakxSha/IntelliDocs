33a42be294771b7802a8d8da61e5e8a7b1721e50
Aakriti Sharma <akksharma474@gmail.com>
2024-11-24 02:45:04 +0530

    Updated Home page. Created a basic navigation for home.

--- documents/views.py
+++ documents/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Welcome to IntelliDocs!")
+    return render(request, 'home.html')
 
 from django.shortcuts import render
 from .forms import DocumentForm
